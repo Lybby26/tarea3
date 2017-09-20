07a572fce6c8171b48e1d23a753131cfe21aa4a6
Mac Mini <macmini@Macs-Mac-mini.local>
2017-09-20 00:04:42 -0500

    Campos de texto Boton de alerta Alert con información capturada

--- Tarea 3/Inicio.h
+++ Tarea 3/Inicio.h
@@ -9,20 +9,34 @@
 #import <UIKit/UIKit.h>
 
 @interface Inicio : UIViewController
+//circulo
 @property (strong, nonatomic) IBOutlet UILabel *lblCirculo;
+
+//Sliders
 @property (strong, nonatomic) IBOutlet UISlider *sliderRed;
 @property (strong, nonatomic) IBOutlet UISlider *sliderGreen;
 @property (strong, nonatomic) IBOutlet UISlider *sliderBlue;
 
+//Sliders labels
 @property (strong, nonatomic) IBOutlet UILabel *lblRed;
-
 @property (strong, nonatomic) IBOutlet UILabel *lblGreen;
-
 @property (strong, nonatomic) IBOutlet UILabel *lblBlue;
 
+//Show and hide circle
 @property (strong, nonatomic) IBOutlet UISwitch *switchOffOn;
 
+//Get a random color
 @property (strong, nonatomic) IBOutlet UIButton *btnRandom;
+
+//Show alert
+@property (strong, nonatomic) IBOutlet UITextField *txtName;
+@property (strong, nonatomic) IBOutlet UITextField *txtPhone;
+@property (strong, nonatomic) IBOutlet UIButton *btnShowAlert;
+
+@property (strong, nonatomic) IBOutlet UILabel *lblNombre;
+
+@property (strong, nonatomic) IBOutlet UILabel *lblPhone;
+
 
 @end
 
--- Tarea 3/Inicio.m
+++ Tarea 3/Inicio.m
@@ -22,40 +22,57 @@
     _lblCirculo.clipsToBounds = YES;
 }
 
+
 - (IBAction)getRedColor:(id)sender {
+    //Mostrar valor decimal del slider rojo
     self.lblRed.text = [NSString stringWithFormat:@"%f", self.sliderRed.value];
-     [self sliderValueChanged:_sliderRed];}
+    
+    //Ejecutar la funcion para cambiar el color del circulo
+    [self sliderValueChanged:_sliderRed];}
 
 
 - (IBAction)getGreenColor:(id)sender {
+    //Mostrar valor decimal del slider verde
     self.lblGreen.text = [NSString stringWithFormat:@"%f", self.sliderGreen.value];
-     [self sliderValueChanged:_sliderGreen];}
+    
+   //Ejecutar la funcion para cambiar el color del circulo
+    [self sliderValueChanged:_sliderGreen];}
 
 
 - (IBAction)getBlueColor:(id)sender {
+    //Mostrar valor decimal del slider azul
     self.lblBlue.text = [NSString stringWithFormat:@"%f", self.sliderBlue.value];
-
+    
+   //Ejecutar la funcion para cambiar el color del circulo
     [self sliderValueChanged:_sliderBlue];
 }
 
-//Cambiar color
+static NSString *lastColor;
+
+//Cambiar color al circulo
 -(void)sliderValueChanged:(UISlider*)slider
 {
-    NSLog(@"%f", _sliderRed.value);
-    NSLog(@"%f", _sliderGreen.value);
-    NSLog(@"%f", _sliderBlue.value);
+   
     
-    float r=[[NSString stringWithFormat:@"%.0f",_sliderRed.value] floatValue];
-    float g=[[NSString stringWithFormat:@"%.0f",_sliderGreen.value]floatValue];
-    float b=[[NSString stringWithFormat:@"%.0f",_sliderBlue.value]floatValue];
+    float redF =[[NSString stringWithFormat:@"%.0f",_sliderRed.value] floatValue];
+    float greenF =[[NSString stringWithFormat:@"%.0f",_sliderGreen.value]floatValue];
+    float blueF =[[NSString stringWithFormat:@"%.0f",_sliderBlue.value]floatValue];
     
-    UIColor *colorToSet=[UIColor colorWithRed:(r/255.0f) green:(g/255.0f) blue:(b/255.0f) alpha:1];
+    NSString *r = [NSString stringWithFormat:@"%f", redF];
+    NSString *g = [NSString stringWithFormat:@"%f", greenF];
+    NSString *b = [NSString stringWithFormat:@"%f", blueF];
+    
+     lastColor = [NSString stringWithFormat:@"(%@,%@,%@)", r, g, b];
+    
+   
+    
+    UIColor *colorToSet=[UIColor colorWithRed:(redF/255.0f) green:(greenF/255.0f) blue:(blueF/255.0f) alpha:1];
     
     NSLog(@"%@", colorToSet);
     [_lblCirculo setBackgroundColor:colorToSet];
 }
 
-
+//Mostrar y ocultar circulo
 - (IBAction)showHideCircle:(id)sender {
    
       if(_switchOffOn.on){
@@ -69,15 +86,76 @@
     }
 
 
+//Elegir un color al azar
 - (IBAction)getARandomColor:(id)sender {
     
     CGFloat red = arc4random() % 255 / 255.0;
     CGFloat green = arc4random() % 255 / 255.0;
     CGFloat blue = arc4random() % 255 / 255.0;
+    
+    
+    
+    NSString *r = [NSString stringWithFormat:@"%f", red];
+    NSString *g = [NSString stringWithFormat:@"%f", green];
+    NSString *b = [NSString stringWithFormat:@"%f", blue];
+    
+    
+    lastColor = [NSString stringWithFormat:@"(%@,%@,%@)", r, g, b];
+    
+    
     UIColor *color = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
     _lblCirculo.backgroundColor = color;
     
 }
+
+//Mostrar un alert view con el contenido de Nombre Telefono y color
+- (IBAction)showAlertWithNameandPhone:(id)sender {
+    
+    //Ocultar  teclado
+    [self.view endEditing:YES];
+    
+    NSString *lblCircleContain = @"No hay circulo";
+    
+     NSString *namePhone = @"";
+    
+    if(_switchOffOn.on){
+        
+        if(lastColor == NULL){
+            lastColor = @"(0,0,0)";
+        }
+        
+        lblCircleContain = lastColor;
+    }
+    
+    NSString *name =  _txtName.text;
+    NSString *phone = _txtPhone.text;
+    
+    
+    if(name == NULL){
+      name= @"";
+    }
+    
+    if(phone == NULL){
+        phone= @"";
+    }
+    
+    
+    
+    namePhone = [NSString stringWithFormat:@"%s:%@%s:%@","Nombre ", name, " Telefono ", phone];
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:lblCircleContain message:namePhone preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+
+    
+  
+}
+
+
+
+
 
 
 
